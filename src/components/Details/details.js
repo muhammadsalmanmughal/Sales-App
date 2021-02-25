c903b416aa03196f5d375a29f39b2fdf6d561c87
muhammadsalmanmughal <meandmylearn@gmail.com>
2021-02-24 19:42:21 -0500

    show vendor/customer details

--- src/components/Details/details.js
+++ src/components/Details/details.js
@@ -1,47 +1,83 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getSpecificData } from '../../Utils/utils'
+import {
+    Divider,
+    Row,
+    Col
+} from 'antd';
 const Details = () => {
+    const [detailsdData, setDetailsData] = useState([])
     const { slug, Cname } = useParams()
     console.log('customerDetails------->', slug, Cname)
 
-    // const getData = () => {
-    //     // const userID = localStorage.getItem('userId')
-    //     // console.log('USER ID============>',userID);
-    //     return firebase
-    //         .firestore()
-    //         .collection(Cname)
-    //         .where("iD", "==", slug)
-    //         .get()
-    //         .then(function (querySnapshot) {
-    //             console.log('querySnapshot', querySnapshot)
-    //             const comlist = [];
-    //             querySnapshot.forEach(function (doc) {
-    //                 if (doc.exists) {
-    //                     const comp = doc.data();
-    //                     comlist.push({ ...comp, compId: doc.id });
-    //                 } else {
-    //                     message.info("No such document!");
-    //                 }
-    //             });
-    //             // setCompanyList(comlist);
-    //             // setInitialCompany(comlist);
-    //             console.log('data-------->', comlist)
-    //             return comlist
-    //         })
-    //         .catch(function (error) {
-    //             console.log("Error getting documents: ", error);
-    //         });
-
-    // }
     useEffect(() => {
-        getSpecificData(slug , Cname)
+        getSpecificData(slug, Cname).then(data => {
+            setDetailsData(data)
+        })
     }, [])
 
-
+    const { city } = detailsdData
     return (
         <div>
-            <h1> Details page</h1>
+            <h2>{Cname} Details page</h2>
+
+            {detailsdData.map(data => {
+                return (
+                    <div>
+                        <Divider orientation="left">
+                            <h2>Name</h2>
+                        </Divider>
+                        <Row gutter={[10, 10]}>
+                            <Col xs={24} sm={16}>
+                                <h2>Name:</h2>
+                                <h3>{data.ownerFirstName +' '+ data.ownerLastName}</h3>
+                            </Col>
+                            <Col xs={24} sm={8}>
+                                <h2>Company:</h2>
+                                <h3>{data.companyName}</h3>
+                            </Col>
+
+                        </Row>
+                        <Divider orientation="left">
+                            <h2>Address</h2>
+                        </Divider>
+                        <Row gutter={[10, 10]}>
+                            <Col xs={24} sm={16}>
+                                <h2>State:</h2>
+                                <h3>State</h3>
+                            </Col>
+                            <Col xs={24} sm={8}>
+                                <h2>City:</h2>
+                                <h3>{data.city}</h3>
+                            </Col>
+                        </Row>
+                        <Row gutter={[10, 10]}>
+                            <Col xs={24} sm={16}>
+                                <h2>Address:</h2>
+                                <h3>{data.address}</h3>
+                            </Col>
+                            <Col xs={24} sm={8}>
+                                <h2>PostalCode:</h2>
+                                <h3>{data.postalCode}</h3>
+                            </Col>
+                        </Row>
+                        <Divider orientation='left'>
+                            <h2>Contact</h2>
+                        </Divider>
+                        <Row gutter={[10, 10]}>
+                            <Col xs={24} sm={16}>
+                                <h2>Email:</h2>
+                                <h3>{data.email}</h3>
+                            </Col>
+                            <Col xs={24} sm={8}>
+                                <h2>Phone Number:</h2>
+                                <h3>{data.phone}</h3>
+                            </Col>
+                        </Row>
+                    </div>
+                )
+            })}
         </div>
     )
 }
