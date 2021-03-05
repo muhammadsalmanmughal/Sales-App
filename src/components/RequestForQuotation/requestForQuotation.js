61fea1df3cce720b8968ab45688c3248e2562057
muhammadsalmanmughal <meandmylearn@gmail.com>
2021-03-04 19:10:12 -0500

    set full date and generate auto id in RFQ

--- src/components/RequestForQuotation/requestForQuotation.js
+++ src/components/RequestForQuotation/requestForQuotation.js
@@ -32,6 +32,7 @@ const RequestForQuatation = () => {
         });
         comlist.map((items, key) => {
           return setAllVendorsName(items)
+          return setAllVendorsName(items.companyName)
         })
         // setAllVendorsName(comlist);
         // setInitialCompany(comlist);
@@ -46,44 +47,61 @@ const RequestForQuatation = () => {
     getAllVendorNames()
   }, [])
 
-  // console.log('allvendorData', allVendorsName)
+  const today = new Date()
+  // let date = today.getDate();
+  // let month = today.getMonth() +1;
+  let year = today.getFullYear();
+  let todayDate = String(today.getDate()).padStart(2, '0')
+  let todayMonth = String(today.getMonth()).padStart(2, '0')
+  const fullDate = `${todayDate}/${todayMonth}/${year}`
+  const shortid = require('shortid');
 
+  console.log(shortid.generate());
   return (
     <div>
       <h1>Request For Quotation</h1>
       <Divider />
-        <Row gutter={[10, 10]}>
-          <Col xs={24} sm={24}>
-            <Label> Compnay Address:
-            <Input 
-            type='text' 
-            disabled 
+      <Row gutter={[10, 10]}>
+        <Col xs={24} sm={12}>
+          <h4>Date: {fullDate}</h4>
+        </Col>
+        <Col xs={24} sm={12}>
+          <h4>RFQ-ID: {shortid.generate()}</h4>
+        </Col>
+      </Row>
+      <Row gutter={[10, 10]}>
+        <Col xs={24} sm={24}>
+          <Label> Compnay Address:
+            <Input
+              type='text'
+              disabled
             />
-            </Label>
-          </Col>
-        </Row>
-        <Divider>Add Items with quantity</Divider>
-        <Row gutter={[10, 10]}>
-          <Col xs={24} sm={10}>
-            <Input 
-            type='text' 
+          </Label>
+        </Col>
+      </Row>
+      <Divider>Add Items with quantity</Divider>
+      <Row gutter={[10, 10]}>
+        <Col xs={24} sm={10}>
+          <Input
+            type='text'
             placeholder='Enter item name'
-            />
-          </Col>
-          <Col xs={24} sm={10}>
-            <Input type='number' placeholder='Enter number of Quantity' />
-          </Col>
-          <Col xs={24} sm={2}>
+          />
+        </Col>
+        <Col xs={24} sm={10}>
+          <Input type='number' placeholder='Enter number of Quantity' />
+        </Col>
+        <Col xs={24} sm={2}>
 
-            <Button>Add</Button>
-          </Col>
+          <Button>Add</Button>
+        </Col>
 
-        </Row>
-        <Row>
-          <Col xs={24} sm={6}>
+      </Row>
+      <Row>
+        <Col xs={24} sm={6}>
           <Button>Create RFQ</Button>
-          </Col>
-        </Row>
+        </Col>
+      </Row>
+
     </div>
   )
 }
